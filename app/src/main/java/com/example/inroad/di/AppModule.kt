4bf1e1e49dd7dd24fb855dabab240d54acfffbc3
Nyurgun Alexeev <nalexeev@sharplogic.ai>
2022-05-09 00:10:21 +0900

    #75 Added accelerometer manager

--- app/src/main/java/com/example/inroad/di/AppModule.kt
+++ app/src/main/java/com/example/inroad/di/AppModule.kt
@@ -3,6 +3,7 @@ package com.example.inroad.di
 import android.content.Context
 import com.example.inroad.managers.LocationManager
 import com.example.inroad.domain.PointInteractor
+import com.example.inroad.managers.AccelerometerManager
 import dagger.Module
 import dagger.Provides
 import javax.inject.Singleton
@@ -20,5 +21,11 @@ class AppModule {
     @Singleton
     fun providesLocationManager(context: Context): LocationManager {
         return LocationManager(context)
+    }
+
+    @Provides
+    @Singleton
+    fun providesAccelerometerManager(context: Context): AccelerometerManager {
+        return AccelerometerManager(context)
     }
 }
---
+++ app/src/main/java/com/example/inroad/managers/AccelerometerManager.kt
@@ -0,0 +1,41 @@
+package com.example.inroad.managers
+
+import android.content.Context
+import android.hardware.Sensor
+import android.hardware.SensorEvent
+import android.hardware.SensorEventListener
+import android.hardware.SensorManager
+import android.location.Location
+import android.util.Log
+import androidx.activity.ComponentActivity
+import io.reactivex.rxjava3.core.Observable
+import io.reactivex.rxjava3.subjects.BehaviorSubject
+
+class AccelerometerManager(
+    context: Context
+) {
+    private lateinit var sensorManager: SensorManager
+
+    private val accelerometerSubject by lazy { BehaviorSubject.create<FloatArray>() }
+
+    val spreads: Observable<FloatArray> = accelerometerSubject
+
+    fun onStart(activity: ComponentActivity) {
+        sensorManager = activity.getSystemService(Context.SENSOR_SERVICE) as SensorManager
+        val listener = object : SensorEventListener {
+            override fun onAccuracyChanged(arg0: Sensor?, arg1: Int) {
+
+            }
+            override fun onSensorChanged(event: SensorEvent) {
+                if (event.sensor.type == Sensor.TYPE_ACCELEROMETER) {
+                    accelerometerSubject.onNext(floatArrayOf(event.values[0], event.values[1], event.values[2]))
+                }
+            }
+        }
+        sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER)?.also {
+            accelerometer ->
+                sensorManager.registerListener(listener, accelerometer,
+                    SensorManager.SENSOR_DELAY_NORMAL, SensorManager.SENSOR_DELAY_UI)
+        }
+    }
+}
