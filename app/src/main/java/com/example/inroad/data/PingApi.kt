eda1f828cc8cbce31371c1a7118233403fa9204f
Anton Chekurov <07anton99@mail.ru>
2022-05-13 19:10:42 +0900

    #76 Add BumpInteractor.kt

---
+++ app/src/main/java/com/example/inroad/data/PingApi.kt
@@ -0,0 +1,14 @@
+package com.example.inroad.data
+
+import retrofit2.Retrofit
+import retrofit2.adapter.rxjava3.RxJava3CallAdapterFactory
+import retrofit2.converter.gson.GsonConverterFactory
+
+class PingApi {
+    private val retrofit = Retrofit.Builder()
+        .baseUrl("http://10.0.2.2/")
+        .addCallAdapterFactory(RxJava3CallAdapterFactory.create())
+        .addConverterFactory(GsonConverterFactory.create())
+        .build()
+
+}
---
+++ app/src/main/java/com/example/inroad/domain/PingInteractor.kt
@@ -0,0 +1,10 @@
+package com.example.inroad.domain
+
+import com.example.inroad.data.PingApi
+
+class PingInteractor {
+    private val ping: PingApi = PingApi()
+
+
+
+}
