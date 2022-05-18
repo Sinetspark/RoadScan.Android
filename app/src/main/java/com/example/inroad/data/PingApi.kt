d22af22e8ddfe8ebab1199e2a3f94003e2b47279
Anton Chekurov <07anton99@mail.ru>
2022-05-18 18:23:54 +0900

    #81 Add PingInteractor.kt

--- app/src/main/java/com/example/inroad/data/PingApi.kt
+++ app/src/main/java/com/example/inroad/data/PingApi.kt
@@ -4,11 +4,16 @@ import retrofit2.Retrofit
 import retrofit2.adapter.rxjava3.RxJava3CallAdapterFactory
 import retrofit2.converter.gson.GsonConverterFactory
 
+
 class PingApi {
+
     private val retrofit = Retrofit.Builder()
-        .baseUrl("http://10.0.2.2/")
+        .baseUrl("https://f5cc-94-245-181-202.jp.ngrok.io/api/")
         .addCallAdapterFactory(RxJava3CallAdapterFactory.create())
         .addConverterFactory(GsonConverterFactory.create())
         .build()
 
+    private val service = retrofit.create(PingService::class.java)
+
+    fun getPing() = service.getPing()
 }
--- app/src/main/java/com/example/inroad/domain/PingInteractor.kt
+++ app/src/main/java/com/example/inroad/domain/PingInteractor.kt
@@ -1,10 +1,12 @@
 package com.example.inroad.domain
 
-import com.example.inroad.data.PingApi
+import com.example.inroad.data.PingRepository
+import com.example.inroad.data.dto.InsertBumps
+import io.reactivex.rxjava3.core.Observable
 
 class PingInteractor {
-    private val ping: PingApi = PingApi()
+    private val repository: PingRepository = PingRepository()
 
-
-
+    fun getPing(): Observable<Void> = repository.getPing()
 }
+
