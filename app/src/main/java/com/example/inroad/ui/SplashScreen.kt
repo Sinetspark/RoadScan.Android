1149dbdd5e4f2afd00a5f822ced64d905ed4bc84
AntonChek <07anton99@mail.ru>
2022-12-12 02:44:29 +0900

    #182 added Splash Screen

---
+++ app/src/main/java/com/example/inroad/ui/SplashScreen.kt
@@ -0,0 +1,23 @@
+package com.example.inroad.ui
+
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import androidx.appcompat.app.AppCompatActivity
+import com.example.inroad.R
+
+class SplashScreen : AppCompatActivity() {
+
+    private val SPLASH_TIME: Long = 3000
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.splash_screen)
+
+        Handler(Looper.getMainLooper()).postDelayed({
+            startActivity(Intent(this, MainActivity::class.java))
+            finish()
+        }, SPLASH_TIME)
+    }
+}
