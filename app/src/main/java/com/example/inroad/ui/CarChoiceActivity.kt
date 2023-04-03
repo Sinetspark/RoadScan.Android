ced21210dd61548414274bb16ed8735cb552c5a6
AntonChek <07anton99@mail.ru>
2023-04-03 18:53:55 +0900

    #157 Change СarChoiceModal

--- app/src/main/java/com/example/inroad/ui/CarChoiceActivity.kt
+++ app/src/main/java/com/example/inroad/ui/CarChoiceActivity.kt
@@ -7,6 +7,7 @@ import android.view.View
 import android.widget.Button
 import android.widget.ImageButton
 import android.widget.ImageView
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.example.inroad.R
 
@@ -14,20 +15,46 @@ import com.example.inroad.R
 class CarChoiceActivity : AppCompatActivity() {
 
     lateinit var choiceBtn: Button
-
+    lateinit var vechicalCarImg: ImageView
+    lateinit var hatchbackCarImg: ImageView
+    lateinit var jeepCarImg: ImageView
+    lateinit var watcherCarImg: ImageView
+    var selectedCar = ""
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.car_choice_modal)
-        val vechicalCar = findViewById<ImageView>(R.id.vechical_car_img)
 
         choiceBtn = findViewById(R.id.idBtnChoice)
-        vechicalCar.setOnClickListener(){
+        vechicalCarImg = findViewById(R.id.vechical_car_img)
+        hatchbackCarImg= findViewById(R.id.hatchback_car_img)
+        jeepCarImg = findViewById(R.id.jeep_car_img)
+        watcherCarImg = findViewById(R.id.watcher_car_img)
+
+        vechicalCarImg.setOnClickListener {
+            selectedCar = "vechical_car"
         }
 
+        hatchbackCarImg.setOnClickListener {
+            selectedCar = "hatchback_car"
+        }
 
+        jeepCarImg.setOnClickListener {
+            selectedCar = "jeep_car"
+        }
+
+        watcherCarImg.setOnClickListener {
+            selectedCar = "watcher"
+        }
 
         choiceBtn.setOnClickListener() {
-            startActivity(Intent(this@CarChoiceActivity, MainActivity::class.java))
+            if (!selectedCar.isNullOrEmpty()) {
+                val intent = Intent(this@CarChoiceActivity, MainActivity::class.java)
+                intent.putExtra("selectedCar", selectedCar)
+                startActivity(intent)
+            } else {
+                Toast.makeText(this@CarChoiceActivity, "Выберите тип автомобиля", Toast.LENGTH_SHORT).show()
+            }
+
         }
     }
 }
